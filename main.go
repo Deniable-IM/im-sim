9d665e5f7695fa15f97bf6250ca7b4b90080770f
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2025-02-11 13:03:59 +0100

    Run Dockerfile

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/archive"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
+
+	buildContext, err := archive.TarWithOptions(".", &archive.TarOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	buildOpt := types.ImageBuildOptions{
+		Dockerfile: "Dockerfile",
+		Tags:       []string{"sdk-test"},
+	}
+
+	buildRes, err := cli.ImageBuild(ctx, buildContext, buildOpt)
+	if err != nil {
+		panic(err)
+	}
+
+	defer buildRes.Body.Close()
+
+	io.Copy(os.Stdout, buildRes.Body)
+
+	containerRes, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "sdk-test",
+		Tty:   true,
+	}, nil, nil, nil, "")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cli.ContainerStart(ctx, containerRes.ID, container.StartOptions{}); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(containerRes.ID)
+
 }
