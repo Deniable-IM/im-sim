3a27ac29ceb970e55b5b3d84341ca7b9c3c96b6a
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2025-02-11 16:20:41 +0100

    Connect to network

--- go.mod
+++ go.mod
@@ -2,11 +2,13 @@ module deniable-im/network-simulation
 
 go 1.23.6
 
+require github.com/docker/docker v27.5.1+incompatible
+
 require (
+	github.com/AdaLogics/go-fuzz-headers v0.0.0-20240806141605-e8a1dd7889d6 // indirect
 	github.com/Microsoft/go-winio v0.4.14 // indirect
 	github.com/containerd/log v0.1.0 // indirect
 	github.com/distribution/reference v0.6.0 // indirect
-	github.com/docker/docker v27.5.1+incompatible // indirect
 	github.com/docker/go-connections v0.5.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
@@ -19,6 +21,8 @@ require (
 	github.com/moby/sys/sequential v0.6.0 // indirect
 	github.com/moby/sys/user v0.3.0 // indirect
 	github.com/moby/sys/userns v0.1.0 // indirect
+	github.com/moby/term v0.5.2 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -26,7 +30,11 @@ require (
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.59.0 // indirect
 	go.opentelemetry.io/otel v1.34.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.34.0 // indirect
 	go.opentelemetry.io/otel/metric v1.34.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.34.0 // indirect
 	go.opentelemetry.io/otel/trace v1.34.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/time v0.10.0 // indirect
+	gotest.tools/v3 v3.5.2 // indirect
 )
--- main.go
+++ main.go
@@ -8,9 +8,15 @@ import (
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/archive"
 )
+
+type Pair[A any, B any] struct {
+	fst A
+	snd B
+}
 
 func main() {
 	ctx := context.Background()
@@ -25,9 +31,10 @@ func main() {
 		panic(err)
 	}
 
+	// Server
 	buildOpt := types.ImageBuildOptions{
-		Dockerfile: "Dockerfile",
-		Tags:       []string{"sdk-test"},
+		Dockerfile: "Dockerfile.server",
+		Tags:       []string{"im-server"},
 	}
 
 	buildRes, err := cli.ImageBuild(ctx, buildContext, buildOpt)
@@ -39,18 +46,111 @@ func main() {
 
 	io.Copy(os.Stdout, buildRes.Body)
 
-	containerRes, err := cli.ContainerCreate(ctx, &container.Config{
-		Image: "sdk-test",
-		Tty:   true,
-	}, nil, nil, nil, "")
+	buildContext, err = archive.TarWithOptions(".", &archive.TarOptions{})
 	if err != nil {
 		panic(err)
 	}
 
-	if err := cli.ContainerStart(ctx, containerRes.ID, container.StartOptions{}); err != nil {
+	// Client 1
+	buildOpt1 := types.ImageBuildOptions{
+		Dockerfile: "Dockerfile.client",
+		Tags:       []string{"im-client-1"},
+	}
+
+	buildRes1, err := cli.ImageBuild(ctx, buildContext, buildOpt1)
+	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(containerRes.ID)
+	defer buildRes1.Body.Close()
 
+	io.Copy(os.Stdout, buildRes1.Body)
+
+	buildContext, err = archive.TarWithOptions(".", &archive.TarOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	// Client 2
+	buildOpt2 := types.ImageBuildOptions{
+		Dockerfile: "Dockerfile.client",
+		Tags:       []string{"im-client-2"},
+	}
+
+	buildRes2, err := cli.ImageBuild(ctx, buildContext, buildOpt2)
+	if err != nil {
+		panic(err)
+	}
+
+	defer buildRes2.Body.Close()
+
+	io.Copy(os.Stdout, buildRes2.Body)
+
+	// Run
+	networkRes, err := cli.NetworkCreate(ctx, "IMvlan", network.CreateOptions{
+		Driver: "macvlan",
+		IPAM: &network.IPAM{
+			Config: []network.IPAMConfig{
+				{
+					Subnet:  "192.168.87.0/24",
+					IPRange: "192.168.87.64/26",
+					Gateway: "192.168.87.1",
+				},
+			},
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// Server
+	containerRes, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "im-server",
+		Tty:   true,
+	}, nil, nil, nil, "im-server")
+	if err != nil {
+		panic(err)
+	}
+
+	// Client 1
+	containerRes1, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "im-client-1",
+		Tty:   true,
+	}, nil, nil, nil, "im-client-1")
+	if err != nil {
+		panic(err)
+	}
+
+	// Client 2
+	containerRes2, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "im-client-2",
+		Tty:   true,
+	}, nil, nil, nil, "im-client-2")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, con := range []Pair[string, string]{
+		{containerRes.ID, "192.168.87.65"},
+		{containerRes1.ID, "192.168.87.70"},
+		{containerRes2.ID, "192.168.87.126"},
+	} {
+		containerID := con.fst
+		ip := con.snd
+
+		if err := cli.ContainerStart(ctx, containerID, container.StartOptions{}); err != nil {
+			panic(err)
+		}
+
+		if err := cli.NetworkConnect(ctx, networkRes.ID, containerID, &network.EndpointSettings{
+			IPAMConfig: &network.EndpointIPAMConfig{
+				IPv4Address: ip,
+			},
+		}); err != nil {
+			panic(err)
+		}
+	}
+
+	fmt.Println("Container ID: ", containerRes.ID)
+	fmt.Println("Network ID: ", networkRes.ID)
 }
