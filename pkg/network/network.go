df312fb4bd1b8414907c95a65bba2b7fd7a3f1b6
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2025-02-13 16:30:19 +0100

    Added network

---
+++ pkg/network/network.go
@@ -0,0 +1,37 @@
+package network
+
+import (
+	"deniable-im/network-simulation/pkg/client"
+	"log"
+
+	"github.com/docker/docker/api/types/network"
+)
+
+type Options struct {
+	Driver string
+	IPAM   *network.IPAM
+}
+
+type Network struct {
+	client  client.Client
+	Name    string
+	ID      string
+	options Options
+}
+
+func NewNetwork(client client.Client, name string, options Options) *Network {
+	inspectRes, err := client.Cli.NetworkInspect(client.Ctx, name, network.InspectOptions{})
+	if err != nil {
+		createRes, err := client.Cli.NetworkCreate(client.Ctx, name, network.CreateOptions{
+			Driver: options.Driver,
+			IPAM:   options.IPAM,
+		})
+		if err != nil {
+			panic(err)
+		} else {
+			return &Network{client: client, Name: name, ID: createRes.ID, options: options}
+		}
+	}
+	log.Printf("Network %s already exists", inspectRes.Name)
+	return &Network{client: client, Name: inspectRes.Name, ID: inspectRes.ID, options: options}
+}
