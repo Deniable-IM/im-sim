54f5208c75c58afb99a307805637ae464649d640
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2025-02-13 16:33:27 +0100

    Refactor

--- cmd/simulate/main.go
+++ cmd/simulate/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"os"
 
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/network"
+	dockerNetwork "github.com/docker/docker/api/types/network"
 
 	"deniable-im/network-simulation/pkg/client"
+	"deniable-im/network-simulation/pkg/container"
 	"deniable-im/network-simulation/pkg/image"
+	"deniable-im/network-simulation/pkg/network"
 )
 
 type Pair[A any, B any] struct {
@@ -24,9 +24,8 @@ func main() {
 		panic(err)
 	}
 	defer client.Close()
-	// defer client.Cli.Close()
 
-	// Server
+	// Build server image
 	serverImage := image.NewImage(*client, ".", types.ImageBuildOptions{
 		Dockerfile: "Dockerfile.server",
 		Tags:       []string{"im-server"},
@@ -40,10 +39,10 @@ func main() {
 
 	io.Copy(os.Stdout, res.Body)
 
-	// Client 1
+	// Build client image
 	clientImage1 := image.NewImage(*client, ".", types.ImageBuildOptions{
 		Dockerfile: "Dockerfile.client",
-		Tags:       []string{"im-client-1"},
+		Tags:       []string{"im-client"},
 	})
 
 	res, err = clientImage1.ImageBuild()
@@ -54,93 +53,41 @@ func main() {
 
 	io.Copy(os.Stdout, res.Body)
 
-	// Client 2
-	clientImage2 := image.NewImage(*client, ".", types.ImageBuildOptions{
-		Dockerfile: "Dockerfile.client",
-		Tags:       []string{"im-client-2"},
-	})
-
-	res, err = clientImage2.ImageBuild()
-	if err != nil {
-		panic(err)
-	}
-	defer res.Body.Close()
-
-	io.Copy(os.Stdout, res.Body)
-
-	// Run
-	var networkID string
-	networkInspect, err := client.Cli.NetworkInspect(client.Ctx, "IMvlan", network.InspectOptions{})
-	if err != nil {
-		networkRes, err := client.Cli.NetworkCreate(client.Ctx, "IMvlan", network.CreateOptions{
-			Driver: "macvlan",
-			IPAM: &network.IPAM{
-				Config: []network.IPAMConfig{
-					{
-						Subnet:  "192.168.87.0/24",
-						IPRange: "192.168.87.64/26",
-						Gateway: "192.168.87.1",
-					},
+	// Create network
+	networkOptions := network.Options{
+		Driver: "macvlan",
+		IPAM: &dockerNetwork.IPAM{
+			Config: []dockerNetwork.IPAMConfig{
+				{
+					Subnet:  "192.168.87.0/24",
+					IPRange: "192.168.87.64/26",
+					Gateway: "192.168.87.1",
 				},
 			},
-		})
-		if err != nil {
-			panic(err)
-		} else {
-			networkID = networkRes.ID
-		}
-	} else {
-		networkID = networkInspect.ID
+		},
 	}
+	network := network.NewNetwork(*client, "IMvlan", networkOptions)
 
-	// Server
-	containerRes, err := client.Cli.ContainerCreate(client.Ctx, &container.Config{
-		Image: "im-server",
-		Tty:   true,
-	}, nil, nil, nil, "")
-	if err != nil {
-		panic(err)
-	}
+	// Create containers
+	server := container.NewContainer(*client, "im-server", "im-server", nil)
+	client1 := container.NewContainer(*client, "im-client", "im-client-1", nil)
+	client2 := container.NewContainer(*client, "im-client", "im-client-2", nil)
 
-	// Client 1
-	containerRes1, err := client.Cli.ContainerCreate(client.Ctx, &container.Config{
-		Image: "im-client-1",
-		Tty:   true,
-	}, nil, nil, nil, "")
-	if err != nil {
-		panic(err)
-	}
-
-	// Client 2
-	containerRes2, err := client.Cli.ContainerCreate(client.Ctx, &container.Config{
-		Image: "im-client-2",
-		Tty:   true,
-	}, nil, nil, nil, "")
-	if err != nil {
-		panic(err)
-	}
-
-	for _, con := range []Pair[string, string]{
-		{containerRes.ID, "192.168.87.65"},
-		{containerRes1.ID, "192.168.87.70"},
-		{containerRes2.ID, "192.168.87.126"},
+	// Run and connect containers to network
+	for _, connectPair := range []Pair[*container.Container, string]{
+		{server, "192.168.87.65"},
+		{client1, "192.168.87.70"},
+		{client2, "192.168.87.126"},
 	} {
-		containerID := con.fst
-		ip := con.snd
+		container := connectPair.fst
+		ip := connectPair.snd
 
-		if err := client.Cli.ContainerStart(client.Ctx, containerID, container.StartOptions{}); err != nil {
+		if err := container.Start(); err != nil {
 			panic(err)
 		}
 
-		if err := client.Cli.NetworkConnect(client.Ctx, networkID, containerID, &network.EndpointSettings{
-			IPAMConfig: &network.EndpointIPAMConfig{
-				IPv4Address: ip,
-			},
-		}); err != nil {
+		if err := container.NetworkConnect(network.ID, ip); err != nil {
 			panic(err)
 		}
 	}
-
-	fmt.Println("Container ID: ", containerRes.ID)
-	fmt.Println("Network ID: ", networkID)
 }
