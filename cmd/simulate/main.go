47178fc831c8043b355e936a2c64a42ebfe9f84d
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2025-02-17 12:38:40 +0100

    Fixed network connect on re-run

--- cmd/simulate/main.go
+++ cmd/simulate/main.go
@@ -80,6 +80,11 @@ func main() {
 	// 	clientContainers = append(clientContainers, clientContainer)
 	// }
 
+    // /21
+    // Min x.x.0.1
+    // Max x.x.7.254
+    // Network reserver x.x.0.0
+    // Range (broadcast) reserved  x.x.0.255
 	// Run and connect containers to network
 	for _, connectPair := range []types.Pair[*container.Container, string]{
 		{Fst: server, Snd: "192.168.87.65"},
@@ -89,12 +94,13 @@ func main() {
 		container := connectPair.Fst
 		ip := connectPair.Snd
 
+        if err := container.NetworkConnect(network.ID, ip); err != nil {
+                panic(err)
+        }
+
 		if err := container.Start(); err != nil {
 			panic(err)
 		}
 
-		if err := container.NetworkConnect(network.ID, ip); err != nil {
-			panic(err)
-		}
 	}
 }
--- pkg/container/container.go
+++ pkg/container/container.go
@@ -7,12 +7,15 @@ import (
 	"log"
 
 	"deniable-im/network-simulation/pkg/types"
+
 	dockerContainer "github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/network"
+
 	dockerNetwork "github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/errdefs"
 	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 )
+
+var ErrNoNetworks = errors.New("container not connected to any network")
 
 type Options struct {
 	containerConfig *dockerContainer.Config
@@ -75,12 +78,17 @@ func NewContainerSlice(client *client.Client, images []types.Pair[string, string
 }
 
 func (container *Container) Start() error {
+	err := container.PruneRedundantNetworks()
+	if err != nil && !errors.Is(err, ErrNoNetworks) {
+		return fmt.Errorf("Container start prune failed: %w", err)
+	}
+
 	if err := container.Client.Cli.ContainerStart(
 		container.Client.Ctx,
 		container.ID,
 		dockerContainer.StartOptions{},
 	); err != nil {
-		return err
+		return fmt.Errorf("Container start failed: %w", err)
 	}
 
 	log.Printf("Container %s started.", container.Name)
@@ -88,25 +96,61 @@ func (container *Container) Start() error {
 }
 
 func (container *Container) NetworkConnect(networkID string, ip string) error {
-	inspect, err := container.Client.Cli.ContainerInspect(container.Client.Ctx, container.ID)
+	_, err := container.Client.Cli.ContainerInspect(container.Client.Ctx, container.ID)
 	if err != nil {
-		return err
+		return fmt.Errorf("Container network connect inspect failed: %w.", err)
 	}
 
-	if _, exists := inspect.NetworkSettings.Networks["IMvlan"]; exists {
-		log.Printf("Container %s already connected to network %s.", container.Name, "IMvlan")
-		return nil
+	err = container.PruneRedundantNetworks()
+	if err != nil && !errors.Is(err, ErrNoNetworks) {
+		return fmt.Errorf("Container network connect prune failed: %w", err)
 	}
 
-	if err := container.Client.Cli.NetworkConnect(container.Client.Ctx, networkID, container.ID, &network.EndpointSettings{
-		IPAMConfig: &network.EndpointIPAMConfig{
+	if err := container.Client.Cli.NetworkConnect(container.Client.Ctx, networkID, container.ID, &dockerNetwork.EndpointSettings{
+		IPAMConfig: &dockerNetwork.EndpointIPAMConfig{
 			IPv4Address: ip,
 		},
 	}); err != nil {
-		return err
+		return fmt.Errorf("Container network connect failed: %w", err)
 	}
 
-	log.Printf("Container %s connected to network %s.", container.Name, "IMvlan")
+	log.Printf("Container %s connected to network %s.", container.Name, networkID)
+	return nil
+}
+
+func (container *Container) GetNetworks() ([]types.Pair[string, string], error) {
+	inspect, err := container.Client.Cli.ContainerInspect(container.Client.Ctx, container.ID)
+	if err != nil {
+		return nil, fmt.Errorf("Container get networks %w", err)
+	}
+
+	var res []types.Pair[string, string]
+	for name, network := range inspect.NetworkSettings.Networks {
+		id := network.NetworkID
+		res = append(res, types.Pair[string, string]{Fst: name, Snd: id})
+	}
+
+	if len(res) == 0 {
+		return nil, ErrNoNetworks
+	}
+
+	return res, nil
+}
+
+func (container *Container) PruneRedundantNetworks() error {
+	networks, err := container.GetNetworks()
+	if err != nil {
+		return fmt.Errorf("Container failed to prune networks %w", err)
+	}
+
+	for _, network := range networks {
+		networkName := network.Fst
+		if err := container.Client.Cli.NetworkDisconnect(container.Client.Ctx, networkName, container.ID, true); err != nil {
+			container.Client.Cli.NetworkRemove(container.Client.Ctx, networkName)
+			log.Printf("Pruned network %s from container %s.", networkName, container.Name)
+		}
+	}
+
 	return nil
 }
 
