7dff82e2daa2728791d5f84293728e1099beb43e
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2025-02-24 09:54:02 +0100

    Added concurrency

--- cmd/stop-sim/main.go
+++ cmd/stop-sim/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 
+	"github.com/docker/docker/api/types"
 	containertypes "github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 )
@@ -21,12 +23,26 @@ func main() {
 		panic(err)
 	}
 
-	for _, container := range containers {
-		fmt.Printf("Stopping container - Running IMAGE: %v\n", container.Names)
-		noWait := 0
-		if err := cli.ContainerStop(ctx, container.ID, containertypes.StopOptions{Timeout: &noWait}); err != nil {
-			panic(err)
+	// c1 := containers[:len(containers)]
+
+	var wg sync.WaitGroup
+	for i, container := range containers {
+		wg.Add(1)
+
+		go func(c types.Container) {
+			defer wg.Done()
+
+			fmt.Printf("Stopping container - Running IMAGE: %v\n", container.Names)
+			noWait := 0
+			if err := cli.ContainerStop(ctx, container.ID, containertypes.StopOptions{Timeout: &noWait}); err != nil {
+				panic(err)
+			}
+		}(container)
+
+		if i%50 == 0 {
+			wg.Wait()
 		}
 	}
 
+	wg.Wait()
 }
