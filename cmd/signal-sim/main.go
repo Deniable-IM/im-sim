d9cc4da56d86ddedacd7036a46e011088410e81a
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2025-03-07 12:02:42 +0100

    Containerize signal

--- cmd/signal-sim/main.go
+++ cmd/signal-sim/main.go
@@ -22,23 +22,53 @@ func main() {
 	}
 	defer dockerClient.Close()
 
+	// Build redis image
+	_, err = image.NewImage(dockerClient, "./cmd/signal-sim/", &image.Options{
+		PullOpt: &image.PullOptions{
+			RefStr: "redis:latest",
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// Build DB image
+	_, err = image.NewImage(dockerClient, "./cmd/signal-sim/", &image.Options{
+		BuildOpt: &dockerTypes.ImageBuildOptions{
+			Dockerfile: "Dockerfile.postgres",
+			Tags:       []string{"im-postgres"},
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
 	// Build server image
-	_, err = image.NewImage(*dockerClient, "./cmd/signal-sim/", dockerTypes.ImageBuildOptions{
-		Dockerfile: "Dockerfile.server",
-		Tags:       []string{"im-server"},
+	_, err = image.NewImage(dockerClient, "./cmd/signal-sim/", &image.Options{
+		BuildOpt: &dockerTypes.ImageBuildOptions{
+			Dockerfile: "Dockerfile.server",
+			Tags:       []string{"im-server"},
+		},
 	})
 	if err != nil {
 		panic(err)
 	}
 
 	// Build client image
-	_, err = image.NewImage(*dockerClient, "./cmd/signal-sim/", dockerTypes.ImageBuildOptions{
-		Dockerfile: "Dockerfile.client",
-		Tags:       []string{"im-client"},
+	_, err = image.NewImage(dockerClient, "./cmd/signal-sim/", &image.Options{
+		BuildOpt: &dockerTypes.ImageBuildOptions{
+			Dockerfile: "Dockerfile.client",
+			Tags:       []string{"im-client"},
+		},
 	})
 	if err != nil {
 		panic(err)
 	}
+
+	// Create network for DB, cache and server
+	networkBackend := network.NewNetwork(dockerClient, "backend", network.Options{
+		Driver: "bridge",
+	})
 
 	// Create network that supports 2046 IPs
 	networkOptions := network.Options{
@@ -55,19 +85,80 @@ func main() {
 	}
 
 	// Create network
-	networkIMvlan := network.NewNetwork(*dockerClient, "IMvlan", networkOptions)
+	networkIMvlan := network.NewNetwork(dockerClient, "IMvlan", networkOptions)
 
-	// Setup Server
+	// Setup redis
+	cache, err := container.NewContainer(
+		dockerClient,
+		"redis:latest",
+		"im-redis",
+		&container.Options{
+			Connections: network.NewConnections(networkBackend),
+			HostConfig: &dockerContainer.HostConfig{
+				Runtime: "crun",
+			},
+			NetworkConfig: &dockerNetwork.NetworkingConfig{
+				EndpointsConfig: map[string]*dockerNetwork.EndpointSettings{
+					"backend": {
+						Aliases: []string{"redis"},
+					},
+				},
+			},
+		})
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cache.Start(); err != nil {
+		panic(err)
+	}
+
+	// Setup DB
+	db, err := container.NewContainer(
+		dockerClient,
+		"im-postgres",
+		"im-postgres",
+		&container.Options{
+			Connections: network.NewConnections(networkBackend),
+			HostConfig: &dockerContainer.HostConfig{
+				Runtime: "crun",
+			},
+			NetworkConfig: &dockerNetwork.NetworkingConfig{
+				EndpointsConfig: map[string]*dockerNetwork.EndpointSettings{
+					"backend": {
+						Aliases: []string{"db"},
+					},
+				},
+			},
+		})
+	if err != nil {
+		panic(err)
+	}
+
+	if err := db.Start(); err != nil {
+		panic(err)
+	}
+
+	// Setup server
 	serverIP := "10.10.248.2"
 	server, err := container.NewContainer(
 		dockerClient,
 		"im-server",
 		"im-server",
 		&container.Options{
-			Network: networkIMvlan,
-			Ipv4:    &serverIP,
+			Connections: network.NewConnections(
+				networkBackend,
+				network.NewAddrMapping(networkIMvlan, serverIP),
+			),
 			HostConfig: &dockerContainer.HostConfig{
 				Runtime: "crun",
+			},
+			NetworkConfig: &dockerNetwork.NetworkingConfig{
+				EndpointsConfig: map[string]*dockerNetwork.EndpointSettings{
+					"IMvlan": {
+						Aliases: []string{"server"},
+					},
+				},
 			},
 		})
 	if err != nil {
@@ -78,7 +169,7 @@ func main() {
 		panic(err)
 	}
 
-	// Setup Clients
+	// Setup clients
 	var images []types.Pair[string, string]
 	for i := range [100]int{} {
 		images = append(images,
@@ -92,6 +183,7 @@ func main() {
 		dockerClient,
 		images,
 		&container.Options{
+			Connections: network.NewConnections(networkIMvlan),
 			HostConfig: &dockerContainer.HostConfig{
 				Runtime: "crun",
 			},
@@ -101,7 +193,7 @@ func main() {
 	}
 
 	reservedIP := []string{"10.10.248.2"}
-	clientContainers, err = container.AssignIP(clientContainers, reservedIP, networkOptions)
+	clientContainers, err = container.AssignIP(clientContainers, reservedIP, *networkIMvlan)
 	if err != nil {
 		panic(err)
 	}
@@ -117,9 +209,6 @@ func main() {
 				panic(err)
 			}
 
-			if err := c.NetworkConnect(networkIMvlan.ID); err != nil {
-				panic(err)
-			}
 		}(client)
 
 		if i%50 == 0 {
