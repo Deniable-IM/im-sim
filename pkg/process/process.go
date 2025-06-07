0174a2f5246fa25137dd2495ea7009ca424d1a1d
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2025-06-07 18:50:53 +0200

    Fixed broken pipe

--- pkg/process/process.go
+++ pkg/process/process.go
@@ -3,10 +3,10 @@ package process
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"net"
 	"runtime"
 	"sync"
-	"time"
 )
 
 var (
@@ -14,37 +14,40 @@ var (
 )
 
 type Process struct {
-	conn   net.Conn
-	Buffer *bytes.Buffer
-	Mu     sync.Mutex
+	conn     net.Conn
+	Buffer   *bytes.Buffer
+	commands []string
+	execFunc func([]string, bool) (*Process, error)
+	mu       sync.Mutex
 }
 
-func NewProcess(conn net.Conn, reader *bytes.Buffer) *Process {
-	return &Process{conn, reader, sync.Mutex{}}
+func NewProcess(conn net.Conn, reader *bytes.Buffer, commands []string, execFunc func([]string, bool) (*Process, error)) *Process {
+	return &Process{conn, reader, commands, execFunc, sync.Mutex{}}
 }
 
 func (process *Process) Cmd(cmd []byte) error {
 	processSem <- struct{}{}
-	time.Sleep(100 * time.Millisecond)
 	defer func() { <-processSem }()
 
-	process.Mu.Lock()
-	defer process.Mu.Unlock()
+	process.mu.Lock()
+	defer process.mu.Unlock()
 
 	_, err := process.conn.Write(cmd)
 	if err != nil {
-		return fmt.Errorf("Process failed to write cmd: %w.", err)
+		err := process.retry(cmd)
+		if err != nil {
+			return fmt.Errorf("Failed to retry process: %w.", err)
+		}
 	}
 	return nil
 }
 
 func (process *Process) Read(delim byte) []string {
 	processSem <- struct{}{}
-	time.Sleep(100 * time.Millisecond)
 	defer func() { <-processSem }()
 
-	process.Mu.Lock()
-	defer process.Mu.Unlock()
+	process.mu.Lock()
+	defer process.mu.Unlock()
 
 	lines := []string{}
 	for process.Buffer.Len() != 0 {
@@ -65,5 +68,28 @@ func (process *Process) Close() error {
 	if process.conn != nil {
 		return process.conn.Close()
 	}
+	return nil
+}
+
+func (process *Process) retry(cmd []byte) error {
+	process.Close()
+
+	newProcess, err := process.execFunc(process.commands, true)
+	if err != nil {
+		return fmt.Errorf("Failed to create new process: %w.", err)
+	}
+	log.Printf("New process created: %v.\n", process.commands)
+
+	process.conn = newProcess.conn
+	process.Buffer = newProcess.Buffer
+	process.commands = newProcess.commands
+	process.execFunc = newProcess.execFunc
+
+	_, err = process.conn.Write(cmd)
+	if err != nil {
+		return fmt.Errorf("New process failed to write: %w.", err)
+	}
+	log.Printf("New process write.")
+
 	return nil
 }
